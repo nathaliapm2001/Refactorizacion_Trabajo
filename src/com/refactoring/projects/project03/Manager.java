b7db4356c8687139725fc37c9504c3a2fbb09a01
Nathalia <nathaliapm2001@gmail.com>
2026-03-03 11:21:38 +0100

    Refactor: Aplicado Encapsulate Field en clase Manager

--- src/com/refactoring/projects/project03/Manager.java
+++ src/com/refactoring/projects/project03/Manager.java
@@ -2,12 +2,44 @@ package com.refactoring.projects.project03;
 
 public class Manager {
 	
-    public String nombre;
-    public double salario;
-    public String departamento;
-    public int empleadosACargo;
+    private String nombre;
+    private double salario;
+    private String departamento;
+    private int empleadosACargo;
     
     public void calcularSalarioAnual() {
-        System.out.println(salario * 12 + empleadosACargo * 1000);
+        System.out.println(getSalario() * 12 + getEmpleadosACargo() * 1000);
     }
+
+	public int getEmpleadosACargo() {
+		return empleadosACargo;
+	}
+
+	public void setEmpleadosACargo(int empleadosACargo) {
+		this.empleadosACargo = empleadosACargo;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
